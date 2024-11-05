81588658b69766b14c43f95eb0256d4155f1a4b0
Mohammad Rizky Pratama <rizkymrp22@gmail.com>
2024-11-05 16:45:15 +0700

    add constant data

---
+++ api-tests/helpers/constants.ts
@@ -0,0 +1,72 @@
+export const ECOSYSTEM_DATA = [
+    {
+        "ecosystem": "Indibiz Agriculture",
+        "subSector": ["Perikanan", "Pertanian"]
+    },
+    {
+        "ecosystem": "Indibiz Ekspedisi",
+        "subSector": ["Others"]
+    },
+    {
+        "ecosystem": "Indibiz Energi",
+        "subSector": ["Sub-Kon Tambang/Minyak Lokal"]
+    },
+    {
+        "ecosystem": "Indibiz Government",
+        "subSector": ["Kementrian BUMN", "Pemerintah"]
+    },
+    {
+        "ecosystem": "Indibiz Health",
+        "subSector": ["Apotek/Lab", "Klinik/Puskesmas", "Praktek Dokter/Bidan", "Rumah Sakit Lokal", "Rumah Sakit Vertikas/RSUP/RSUPN (Swasta)"]
+    },
+    {
+        "ecosystem": "Indibiz Hotel",
+        "subSector": ["Hotel non BUMN", "Objek Wisata", "Villa/Resort/Bungalow"]
+    },
+    {
+        "ecosystem": "Indibiz Logisitik",
+        "subSector": ["Transportasi Darat/Laut/Udara milik BUMN"]
+    },
+    {
+        "ecosystem": "Indibiz Manufaktur",
+        "subSector": ["Kontraktor Lokal", "Perusahaan Manufacture"]
+    },
+    {
+        "ecosystem": "Indibiz Media & Komunikasi",
+        "subSector": ["Event, Entertaiment & MICE", "Event, Entertainment & MICE", "Event, entertainment & MICE", "Percetakan/Advertising/Iklan/Digital Printing", "Stasiun Radio Lokal"]
+    },
+    {
+        "ecosystem": "Indibiz Multifinance",
+        "subSector": ["Bank Perkreditan Rakyat (BPR)", "Koperasi", "Leasing"]
+    },
+    {
+        "ecosystem": "Indibiz Other",
+        "subSector": ["Others"]
+    },
+    {
+        "ecosystem": "Indibiz RUKO",
+        "subSector": ["Bengkel/Jasa Perbaikan atau Service", "Car Wash", "Dealer/Distributor/Agen/Grosir", "Design Grafis/Interior/Studio Foto", "Konsultan dan Biro Jasa", "Laundry", "Minimarket", "Petshop", "Rumah Ibadah/Organisasi", "Rumah Makan/Cafe", "Salon/Makeup Artist/Spa", "Warnet/Game"]
+    },
+    {
+        "ecosystem": "Indibiz Sekolah",
+        "subSector": ["PTN, Sekolah Tinggi/Universitas Pertahanan", 
+        "Pendidikan Dasar (TK/SD/SLB/PAUD)", 
+        "Pendidikan Menengah (SMP/SMA/SMK)",
+        "Pendidikan Tinggi (Akademi, Sekolah Tinggi/Universitas)"]
+    }
+];
+
+export const expectedColors = {
+    'Data Mentah': '#5A666D',
+    'Scrapping Google': '#6A4800',
+    'Proses Survey': '#535E98',
+    'Assigned': '#535E98',
+    'Proses Approval - POI Hasil Survei': '#535E98',
+    'Proses Approval - POI Baru': '#535E98',
+    'Invalid': '#6A4800',
+    'Valid Mitra': '#157631',
+    'Valid Internal': '#157631',
+    'submitted': '#157631',
+    'Valid': '#157631',
+    'Not Found': '#C72037'
+};
