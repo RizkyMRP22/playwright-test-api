c09f76b89c2e3274b3a603f522116eb2d589b0c7
Mohammad Rizky Pratama <rizkymrp22@gmail.com>
2024-11-06 11:53:37 +0700

    fixing logic parsing data

--- api-tests/helpers/parsingData.ts
+++ api-tests/helpers/parsingData.ts
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 export const saveStorage = (fileName: string, token: string): void => {
-    const TOKEN_PATH = path.resolve(__dirname, `../localStorage/${fileName}.localStorage.json`);
-    fs.writeFileSync(TOKEN_PATH, JSON.stringify({ [fileName] : token }));
+    const FILE_PATH = path.resolve(__dirname, `../localStorage/${fileName}.localStorage.json`);
+    fs.writeFileSync(FILE_PATH, JSON.stringify({ [fileName]: token }, null, 2));
 }
 
 export const getStorage = (fileName: string): string => {
-    const TOKEN_PATH = path.resolve(__dirname, `../localStorage/${fileName}.localStorage.json`);
-    const token = JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf-8'));
+    const FILE_PATH = path.resolve(__dirname, `../localStorage/${fileName}.localStorage.json`);
+    const token = JSON.parse(fs.readFileSync(FILE_PATH, 'utf-8'));
     return token[fileName];
 }
