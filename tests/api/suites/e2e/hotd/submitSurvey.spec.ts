5deae738accdcc0217b0ad5ef549354521ba9922
rizkymrp22 <rizkymrp22@gmail.com>
2024-11-28 14:27:13 +0700

    add case get list survey

--- tests/api/suites/e2e/hotd/submitSurvey.spec.ts
+++ tests/api/suites/e2e/hotd/submitSurvey.spec.ts
@@ -1,30 +1,39 @@
 import { test, expect, APIRequestContext } from '@playwright/test';
 import { login } from '../../../endpoints/auth/postLogin';
+import { approveSurveyStep } from '../../../scenarios/approval/postApprovalSurvey.ts';
 import { getListPoi } from '../../../endpoints/poi/getListPoi';
-import { postAssignPoiHOTD } from '../../../endpoints/poi/postAssignPoi';
 import { getPoiDetail } from '../../../endpoints/poi/getPoiDetail';
+import { getSummaryPoi } from '../../../endpoints/poi/getSummaryPoi';
+import { getListSurvey } from '../../../endpoints/approval/getListSurvey';
+import { postAssignPoiHOTD } from '../../../endpoints/poi/postAssignPoi';
 import { postSubmitSurveyPoi } from '../../../endpoints/poi/postSubmitSurvey';
 import { postUploadEvidence } from '../../../endpoints/poi/postUploadEvidence';
 import { getStorage, saveStorage } from '../../../../../helpers/parsingData';
-import { approveSurveyStep } from '../../../scenarios/approval/postApprovalSurvey.ts';
-import { getSummaryPoi } from '../../../endpoints/poi/getSummaryPoi';
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 test.describe.serial('[E2E] Profiling POI By HOTD', () => {
     let loginToken: string;
+    let loginTokenMGR: string;
     let poiId: string;
     let email: string;
     let respondentId: string;
-    let summaryPoi: any;
+    interface SummaryPoi {
+        unvalidated: number;
+        assigned: number;
+        approvalProcessValidInternal: number;
+        valid: number;
+    }
+
+    let summaryPoi: SummaryPoi;
 
     test.beforeAll(async ({ request }: { request: APIRequestContext }) => {
         const response = await login(request);
         const responseData = await response.json();
 
-        expect(response.ok()).toBeTruthy();
-        expect(responseData.code).toBe(200);
-        expect(responseData.message).toBe("Your Request Has Been Processed");
+        expect.soft(response.ok()).toBeTruthy();
+        expect.soft(responseData.code).toBe(200);
+        expect.soft(responseData.message).toBe("Your Request Has Been Processed");
 
         loginToken = responseData.data.accessToken;
         email = responseData.data.email;
@@ -40,74 +49,71 @@ test.describe.serial('[E2E] Profiling POI By HOTD', () => {
         expect.soft(response.ok(), 'Expected response API to be valid').toBeTruthy();
         const responseData = await response.json();
         summaryPoi = responseData.data;
+        console.log(summaryPoi)
     });
 
     test('Get List POI with Data Mentah', async ({ request }: { request: APIRequestContext }) => {
         const params = {
             page: 1,
             size: 10,
-            sort: "desc"
+            sort: "desc",
+            status: "dataMentah"
         };
-    
+
         const response = await getListPoi(request, loginToken, params);
-        const responseData = await response.json();    
+        const responseData = await response.json();
         expect.soft(response.ok(), 'Expected response API is valid').toBeTruthy();
         if (responseData.data.length > 1) {
-        // Select a random POI index
-        const randomIndex = Math.floor(Math.random() * responseData.data.length);
-        const selectedPoi = responseData.data[randomIndex];
-            poiId = responseData.data[0].idPoi;
+            const randomIndex = Math.floor(Math.random() * responseData.data.length);
+            const selectedPoi = responseData.data[randomIndex];
+            poiId = selectedPoi.idPoi;
             console.log(`Randomly selected POI ID: ${selectedPoi.idPoi} random index ${randomIndex}`);
         } else {
             console.warn('Expected more than 1 record but got:', responseData.data.length);
         }
-    });``
-      
+    }); ``
+
     test('Assignment POI', async ({ request }) => {
-    if (!poiId) {
-        throw new Error('POI ID is not available. Ensure the previous test ran successfully.');
-    }
-    
-    const payload = {
-        poiId,
-        emailUserAgent: email,
-        assignTo: 'HOTD',
-        assignmentType: 'validasi',
-    };
-    
-    console.log('Assignment Payload:', payload);
-    
-    const responseAssign = await postAssignPoiHOTD(request, loginToken, payload);
-    const responseDataAssign = await responseAssign.json();
-    
-    // Validate assignment response
-    expect(responseDataAssign.message, 'Expected Message POI berhasil diassign').toBe('POI berhasil diassign');
+        if (!poiId) {
+            throw new Error('POI ID is not available. Ensure the previous test ran successfully.');
+        }
+
+        const payload = {
+            poiId,
+            emailUserAgent: email,
+            assignTo: 'HOTD',
+            assignmentType: 'validasi',
+        };
+
+        console.log('Assignment Payload:', payload);
+
+        const responseAssign = await postAssignPoiHOTD(request, loginToken, payload);
+        const responseDataAssign = await responseAssign.json();
+        expect.soft(responseDataAssign.message, 'Expected Message POI berhasil diassign').toBe('POI berhasil diassign');
     });
 
-    test('Get Summary POI - After Assignment POI', async ({ request }: { request: APIRequestContext }) => {
+    test('[FIX] Get Summary POI - After Assignment POI', async ({ request }: { request: APIRequestContext }) => {
+        await delay(2000)
         const response = await getSummaryPoi(request, loginToken);
         expect.soft(response.ok(), 'Expected response API to be valid').toBeTruthy();
         const responseData = await response.json();
         const summaryPoiExisting = summaryPoi;
         const expectedUnvalidatedPoi = summaryPoiExisting.unvalidated - 1;
         const expectedAssignedPoi = summaryPoiExisting.assigned + 1;
-    
-        // Debug logs
+
         console.log('Existing unvalidated POI:', summaryPoiExisting.unvalidated);
         console.log('Expected unvalidated POI:', expectedUnvalidatedPoi);
         console.log('Existing assigned POI:', summaryPoiExisting.assigned);
         console.log('Expected assigned POI:', expectedAssignedPoi);
-    
-        // Assertions for unvalidated POI
+
         expect.soft(
             responseData.data.unvalidated.toString(),
             `Expected unvalidated POI count updated, from ${summaryPoiExisting.unvalidated} to ${expectedUnvalidatedPoi}`
         ).toBe(expectedUnvalidatedPoi.toString());
-    
-        // Assertions for assigned POI
+
         expect.soft(
             responseData.data.assigned.toString(),
-            'Expected assigned POI count to updated'
+            `Expected assigned POI count to updated, from ${summaryPoiExisting.assigned} to ${expectedAssignedPoi}`
         ).toBe(expectedAssignedPoi.toString());
     });
 
@@ -115,8 +121,8 @@ test.describe.serial('[E2E] Profiling POI By HOTD', () => {
         const responsePoiDetail = await getPoiDetail(request, loginToken, poiId);
         const responseDataPoiDetail = await responsePoiDetail.json();
 
-        expect(responseDataPoiDetail.data.idPoi).toBe(poiId);
-        expect(responseDataPoiDetail.data.status[0].label).toBe("Proses Survey");
+        expect.soft(responseDataPoiDetail.data.idPoi).toBe(poiId);
+        expect.soft(responseDataPoiDetail.data.status[0].label).toBe("Proses Survey");
 
         await delay(5000);
     });
@@ -132,7 +138,7 @@ test.describe.serial('[E2E] Profiling POI By HOTD', () => {
             responseUploadEvidence = await postUploadEvidence(request, loginToken, poiId);
             if (responseUploadEvidence.ok()) {
                 responseDataUploadEvidence = await responseUploadEvidence.json();
-                expect(responseDataUploadEvidence.data.mysiisPhotoId).toBeDefined();
+                expect.soft(responseDataUploadEvidence.data.mysiisPhotoId).toBeDefined();
                 saveStorage("evidence-upload", JSON.stringify(responseDataUploadEvidence.data));
                 break;
             } else {
@@ -163,58 +169,68 @@ test.describe.serial('[E2E] Profiling POI By HOTD', () => {
         const responseDataSubmitSurvey = await responseSubmitSurvey.json();
         console.log(responseDataSubmitSurvey);
 
-        expect(responseSubmitSurvey.ok()).toBeTruthy();
-        expect(responseDataSubmitSurvey.data.respodentId).toBeDefined();
-
+        expect.soft(responseSubmitSurvey.ok()).toBeTruthy();
         respondentId = responseDataSubmitSurvey.data.respodentId;
-        saveStorage("respondentId", JSON.stringify(respondentId));
+        // saveStorage("respondentId", JSON.stringify(respondentId));
         await delay(1000);
     });
 
-    test('Validate POI detail has status Valid Internal', async ({ request }) => {
+    test('[FIX] Validate POI detail has status Valid Internal', async ({ request }) => {
         await delay(1000);
         const responsePoiDetail = await getPoiDetail(request, loginToken, poiId);
         const responseDataPoiDetail = await responsePoiDetail.json();
-        console.log(responseDataPoiDetail);
-
-        expect(responseDataPoiDetail.data.status[0].label).toBe("Proses Approval - POI Hasil Survei");
-        expect(responseDataPoiDetail.data.status[1].label).toBe("Valid Internal");
-        expect(responseDataPoiDetail.data.idPoi).toBe(poiId);
-        expect(responseDataPoiDetail.data.respondentId).toBe(Number(respondentId));
+        expect.soft(responseDataPoiDetail.data.status[0].label).toBe("Proses Approval - POI Hasil Survei");
+        expect.soft(responseDataPoiDetail.data.status[1].label).toBe("Valid Internal");
+        expect.soft(responseDataPoiDetail.data.idPoi).toBe(poiId);
     });
 
-    test('Get Summary POI - After Submit Survey POI', async ({ request }: { request: APIRequestContext }) => {
+    test('[FIX] Get Summary POI - After Submit Survey POI', async ({ request }: { request: APIRequestContext }) => {
+        await delay(2000)
         const response = await getSummaryPoi(request, loginToken);
         expect.soft(response.ok(), 'Expected response API to be valid').toBeTruthy();
         const responseData = await response.json();
         const summaryPoiExisting = summaryPoi;
-        const expectedAssignedPoi = summaryPoiExisting.assigned - 1;
+        const expectedAssignedPoi = summaryPoiExisting.assigned;
         const expectedapprovalProcessValidInternal = summaryPoiExisting.approvalProcessValidInternal + 1;
-    
-        // Debug logs
+
         console.log('Existing assigned POI:', summaryPoiExisting.assigned);
         console.log('Expected assigned POI:', expectedAssignedPoi);
         console.log('Existing valid internal POI:', summaryPoiExisting.approvalProcessValidInternal);
         console.log('Expected valid internal POI:', expectedapprovalProcessValidInternal);
-    
-        // Assertions for assigned POI
+
         expect.soft(
             responseData.data.assigned.toString(),
             `Expected assigned POI count updated, from ${summaryPoiExisting.assigned} to ${expectedAssignedPoi}`
         ).toBe(expectedAssignedPoi.toString());
-    
-        // Assertions for valid internal POI
+
         expect.soft(
             responseData.data.approvalProcessValidInternal.toString(),
-            'Expected assigned POI count to updated'
+            `Expected Valid Internal POI count to updated, From ${summaryPoiExisting.approvalProcessValidInternal} to ${expectedapprovalProcessValidInternal}`
         ).toBe(expectedapprovalProcessValidInternal.toString());
     });
 
-    test('Approval Survey by MGR Witel', async ({ request }) => {
+    test('Validate POI with Valid Internal status show in MGR Witel', async ({ request }) => {
         const nik = '850162dummy';
-        const response = await login(request, nik);
-        const responseData = await response.json();
-        loginToken = responseData.data.accessToken;
+        const responseLogin = await login(request, nik);
+        const responseData = await responseLogin.json();
+        loginTokenMGR = responseData.data.accessToken;
+
+        const payload = {
+            page: 1,
+            size: 10,
+            sortBy: "createdDate",
+            sort: "desc"
+        };
+        const response = await getListSurvey(request, loginTokenMGR, payload);        
+        const responseDataListSurvey = await response.json();
+
+        const isIdPoiPresent = responseDataListSurvey.data.some((poi:any) => poi.idPoi === poiId);
+        console.log(`make sure ${poiId} is ${isIdPoiPresent}`)
+        expect.soft(isIdPoiPresent,`Expect POI id ${poiId} has show in approval survey list`).toBe(true)
+
+    });
+
+    test('Approval Survey by MGR Witel', async ({ request }) => {
 
         const payload = {
             action: "valid",
@@ -223,34 +239,31 @@ test.describe.serial('[E2E] Profiling POI By HOTD', () => {
             poiId: poiId
         }
         console.log(payload);
-        await approveSurveyStep(request, loginToken, payload);
+        await approveSurveyStep(request, loginTokenMGR, payload);
     });
 
     test('Get Summary POI - After Approval MGR Witel', async ({ request }: { request: APIRequestContext }) => {
-        const response = await getSummaryPoi(request, loginToken);
+        await delay(2000)
+        const response = await getSummaryPoi(request, loginTokenMGR);
         expect.soft(response.ok(), 'Expected response API to be valid').toBeTruthy();
         const responseData = await response.json();
         const summaryPoiExisting = summaryPoi;
-        const expectedapprovalProcessValidInternal = summaryPoiExisting.approvalProcessValidInternal - 1;
-        const expectedvalid= summaryPoiExisting.valid + 1;
+        const expectedapprovalProcessValidInternal = summaryPoiExisting.approvalProcessValidInternal;
+        const expectedvalid = summaryPoiExisting.valid + 1;
 
-    
-        // Debug logs
-        console.log('Existing assigned POI:', summaryPoiExisting.valid);
-        console.log('Expected assigned POI:', expectedvalid);
+        console.log('Existing Valid POI:', summaryPoiExisting.valid);
+        console.log('Expected Valid POI:', expectedvalid);
         console.log('Existing valid internal POI:', summaryPoiExisting.approvalProcessValidInternal);
         console.log('Expected valid internal POI:', expectedapprovalProcessValidInternal);
-    
-        // Assertions for valid POI
+
         expect.soft(
-            responseData.data.assigned.toString(),
-            `Expected assigned POI count updated, from ${summaryPoiExisting.valid} to ${expectedvalid}`
+            responseData.data.valid.toString(),
+            `Expected Valid POI count updated, from ${summaryPoiExisting.valid} to ${expectedvalid}`
         ).toBe(expectedvalid.toString());
-    
-        // Assertions for valid internal POI
+
         expect.soft(
             responseData.data.approvalProcessValidInternal.toString(),
-            'Expected assigned POI count to updated'
+            `Expected Valid Internal POI count to updated, from ${summaryPoiExisting.approvalProcessValidInternal} to ${expectedapprovalProcessValidInternal}`
         ).toBe(expectedapprovalProcessValidInternal.toString());
     });
 });
