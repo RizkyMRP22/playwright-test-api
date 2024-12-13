e77390183ace6a07f21348719b1c1988561b7836
rizkymrp22 <rizkymrp22@gmail.com>
2024-12-13 09:57:14 +0700

    enhance case in baseTestCase

--- helpers/baseTestCase.ts
+++ helpers/baseTestCase.ts
@@ -1,4 +1,5 @@
 import { expect } from '@playwright/test';
+import Ajv from 'ajv';
 
 class BaseTestCase {
   /**
@@ -12,18 +13,30 @@ class BaseTestCase {
       expected: any;
       useSoft?: boolean;
       contains?: boolean; // Indicates "contains" check
-      strictEqual?: boolean; // New property for strict equality check
+      strictEqual?: boolean; // Indicates strict equality check
     }>,
     responseData: any
   ): void {
-    assertionObjects.forEach(({ message, actual, expected, useSoft = false, contains = false, strictEqual = false }) => {
+    const functionName = (new Error()).stack?.split('\n')[2]?.trim().split(' ')[1] || 'UnknownFunction';
+
+    assertionObjects.forEach(({
+      message,
+      actual,
+      expected,
+      useSoft = false,
+      contains = false,
+      strictEqual = false,
+    }) => {
       try {
+        let assertionPassed = false;
+
         if (contains) {
           // Check if "actual" contains "expected"
           const match = expected instanceof Array
             ? expected.some((val) => actual.includes(val))
             : actual.includes(expected);
-  
+
+          assertionPassed = match;
           if (useSoft) {
             expect.soft(match, message).toBeTruthy();
           } else {
@@ -31,36 +44,52 @@ class BaseTestCase {
           }
         } else if (strictEqual) {
           // Handle strict equality assertions
+          assertionPassed = JSON.stringify(actual) === JSON.stringify(expected);
           if (useSoft) {
             expect.soft(actual, message).toStrictEqual(expected);
           } else {
             expect(actual, message).toStrictEqual(expected);
           }
-        } else if (useSoft) {
+        } else {
           // Handle regular assertions
           if (expected instanceof RegExp) {
-            expect.soft(actual, message).toMatch(expected);
+            assertionPassed = expected.test(actual);
+            if (useSoft) {
+              expect.soft(actual, message).toMatch(expected);
+            } else {
+              expect(actual, message).toMatch(expected);
+            }
           } else {
-            expect.soft(actual, message).toBe(expected);
-          }
-        } else {
-          if (expected instanceof RegExp) {
-            expect(actual, message).toMatch(expected);
-          } else {
-            expect(actual, message).toBe(expected);
+            assertionPassed = actual === expected;
+            if (useSoft) {
+              expect.soft(actual, message).toBe(expected);
+            } else {
+              expect(actual, message).toBe(expected);
+            }
           }
         }
+
+        // Log only if assertion failed
+        if (!assertionPassed) {
+          this.logger('error', `Assertion failed in ${functionName}: ${message}`, {
+            actual,
+            expected,
+          });
+        }
+
       } catch (error) {
-        console.error(`Assertion failed: ${message}`);
-        console.info('Debugging Info:');
-        console.info(`- Expected: ${JSON.stringify(expected, null, 2)}`);
-        console.info(`- Actual: ${JSON.stringify(actual, null, 2)}`);
-        console.info('- Response Data:', JSON.stringify(responseData, null, 2));
+        // Enhanced logging for exceptions
+        this.logger('error', `Assertion threw an error in ${functionName}: ${message}`, {
+          actual,
+          expected,
+          error: error.message,
+        });
+
+        // Re-throw the error to ensure test fails
         throw error;
       }
     });
   }
-  
 
   /**
    * Perform multiple "defined" assertions.
@@ -74,6 +103,8 @@ class BaseTestCase {
     }>,
     responseData: any
   ): void {
+    const functionName = (new Error()).stack?.split('\n')[2]?.trim().split(' ')[1] || 'UnknownFunction';
+  
     assertionObjects.forEach(({ message, actual, useSoft = false }) => {
       try {
         if (useSoft) {
@@ -82,37 +113,90 @@ class BaseTestCase {
           expect(actual, message).toBeDefined();
         }
       } catch (error) {
-        console.error(`Assertion failed: ${message}`);
-        console.info('Debugging Info:');
-        console.info(`- Actual: ${JSON.stringify(actual, null, 2)}`);
-        console.info('- Response Data:', JSON.stringify(responseData, null, 2));
+        // Enhanced logging for failed assertions
+        this.logger('error', `Assertion failed in ${functionName}: ${message}`, {
+          actual,
+          responseData,
+          error: error.message,
+        });
+  
+        // Additional debugging info
+        this.logger('info', `Debugging Info for ${functionName}`, {
+          message,
+          actual, 
+          responseData
+        });
+  
+        // Re-throw the error to ensure test fails
         throw error;
       }
-    });
+    });    
   }
 
-    /**
-   * Logger function to standardize log output.
-   * @param type - The type of log (info, warn, error).
-   * @param message - The message to log.
-   * @param data - Optional additional data to include in the log.
-   */
-    static logger(type: 'info' | 'warn' | 'error', message: string, data?: any): void {
-      const timestamp = new Date().toISOString();
-      switch (type) {
-        case 'info':
-          console.info(`[INFO] ${message}`, JSON.stringify(data,null,2) || '');
-          break;
-        case 'warn':
-          console.warn(`[WARN] ${timestamp} - ${message} \n`, JSON.stringify(data,null,2) || '');
-          break;
-        case 'error':
-          console.error(`[ERROR] ${timestamp} - ${message} \n`, JSON.stringify(data,null,2) || '');
-          break;
-        default:
-          console.log(`[LOG] ${timestamp} - ${message} \n`, JSON.stringify(data,null,2) || '');
-      }
+  /**
+ * Logger function to standardize log output.
+ * @param type - The type of log (info, warn, error).
+ * @param message - The message to log.
+ * @param data - Optional additional data to include in the log.
+ */
+  static logger(type: 'info' | 'warn' | 'error', message: string, data?: any): void {
+    const timestamp = new Date().toISOString();
+    switch (type) {
+      case 'info':
+        console.info(`[INFO] ${message}`, JSON.stringify(data, null, 2) || '');
+        break;
+      case 'warn':
+        console.warn(`[WARN] ${timestamp} \n ${message} \n`, JSON.stringify(data, null, 2) || '');
+        break;
+      case 'error':
+        console.error(`[ERROR] ${timestamp} \n ${message} \n`, JSON.stringify(data, null, 2) || '');
+        break;
+      default:
+        console.log(`[LOG] ${timestamp} \n ${message} \n`, JSON.stringify(data, null, 2) || '');
     }
+  }
+
+  static assertSchema(responseData: any, schema: any) {
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(schema);
+    const valid = validate(responseData);
+
+    if (!valid) {
+      // Process validation errors
+      const errors = validate.errors?.map(error => {
+        const actualValue = error.data !== undefined ? error.data : 'undefined';
+        const actualType = typeof actualValue;
+
+        // Handle missing required fields
+        if (error.keyword === 'required') {
+          return {
+            path: error.instancePath || '(root)',
+            message: `Missing required field: ${error.params.missingProperty}`,
+          };
+        }
+
+        // Handle additional fields in the response that are not in the schema
+        if (error.keyword === 'additionalProperties') {
+          return {
+            path: error.instancePath || '(root)',
+            message: `Unexpected field in response: ${error.params.additionalProperty}`,
+          };
+        }
+
+        // Handle type mismatches
+        const expectedType = error.params.type || 'unknown';
+        return {
+          path: error.instancePath || '(root)',
+          actualType,
+          expectedType,
+          message: `Please fix data type from ${actualType} to ${expectedType}`,
+        };
+      });
+
+      this.logger('error', 'Validation JSON Schema errors details:', errors);
+    }
+    expect(valid, 'Expected Schema is valid').toBeTruthy();
+  }
 }
 
 export default BaseTestCase;
